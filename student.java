ca895e3f9c78af48dee5f8cf2e5f7f50f4cb6269
sang-boi <sangbsg59@gmail.com>
2026-02-16 10:28:51 +0530

    Create student.java

---
+++ student.java
@@ -0,0 +1,21 @@
+package studentpack;
+
+public class student {
+	private String name;
+	private int regNo;
+	
+	public void setName(String n) {
+		name = n;
+	}
+	public void setRegNo(int r) {
+		regNo = r;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getRegNo() {
+		return regNo;
+		
+	}
+
+}
